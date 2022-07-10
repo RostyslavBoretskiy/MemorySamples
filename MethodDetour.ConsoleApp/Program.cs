6d3ba0407368146008bcd16b8d92362a58b095c8
rostyslav.boretskiy@gmail.com <rostyslav.boretskiy@gmail.com>
2022-07-10 22:33:42 +0300

    Add project files.

---
+++ MethodDetour.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using MethodDetour.MyAttempt;
+using System;
+
+namespace MethodDetour.ConsoleApp;
+
+class Program
+{
+    public static void Main()
+    {
+        SomeNotAllovedClassForModify.DoSmth("Hello world"); // Origin: Hello world
+
+        var source = typeof(SomeNotAllovedClassForModify)
+            .GetMethod(nameof(SomeNotAllovedClassForModify.DoSmth)); // "DoSmth"
+        var dest = typeof(OurOverridedMethod)
+            .GetMethod(nameof(OurOverridedMethod.DoSmth)); // "DoSmth"
+
+        MemoryReplacer.DetourMethod(source, dest);
+
+        SomeNotAllovedClassForModify.DoSmth("Hello world"); // Overrided: Hello world
+    }
+}
+
+sealed class SomeNotAllovedClassForModify
+{
+    public static void DoSmth(string arg)
+    {
+        Console.WriteLine("Original: " + arg);
+    }
+}
+
+class OurOverridedMethod
+{
+    public static void DoSmth(string arg)
+    {
+        Console.WriteLine("Overrided: " + arg);
+    }
+}
---
+++ MethodDetour.MyAttempt/MemoryReplacer.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace MethodDetour.MyAttempt;
+
+public unsafe static class MemoryReplacer
+{
+    public static void DetourMethod(MethodBase source, MethodBase destination)
+    {
+        var sourcePtr = (byte*)source.MethodHandle.GetFunctionPointer().ToPointer();
+        var destPtr = (byte*)destination.MethodHandle.GetFunctionPointer().ToPointer();
+
+        *(Int64*)(sourcePtr + 1) = destPtr - (sourcePtr + 0x1 + 0x4);
+    }
+}
